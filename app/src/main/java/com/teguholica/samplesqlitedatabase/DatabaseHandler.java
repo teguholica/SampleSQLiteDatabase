609af9a62f8d64cef461bb1cdc8a8a721f90c3a1
Teguh Arifianto <teguholica@gmail.com>
2017-01-09 22:43:09 +0700

    Initial commit

---
+++ app/src/main/java/com/teguholica/samplesqlitedatabase/DatabaseHandler.java
@@ -0,0 +1,125 @@
+package com.teguholica.samplesqlitedatabase;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by teguholica on 09/01/2017.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactsManager";
+    private static final String TABLE_CONTACTS = "contacts";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PH_NO = "phone_number";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_PH_NO + " TEXT" + ")";
+        sqLiteDatabase.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PH_NO, contact.getPhoneNumber());
+
+        db.insert(TABLE_CONTACTS, null, values);
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        Cursor cursor = db.query(
+                TABLE_CONTACTS,
+                new String[]{KEY_ID, KEY_NAME, KEY_PH_NO},
+                KEY_ID + "=?",
+                new String[]{String.valueOf(id)},
+                null, null, null, null);
+
+        db.close();
+        cursor.close();
+
+        return new Contact(
+                cursor.getInt(0),
+                cursor.getString(1),
+                cursor.getString(2)
+        );
+    }
+
+    public List<Contact> getAllContacts() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        List<Contact> contactList = new ArrayList<>();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_CONTACTS, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                contactList.add(new Contact(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2)
+                ));
+            } while (cursor.moveToNext());
+        }
+
+        db.close();
+        cursor.close();
+
+        return contactList;
+    }
+
+    public int getContactsCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_CONTACTS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        return cursor.getCount();
+    }
+
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PH_NO, contact.getPhoneNumber());
+
+        // updating row
+        return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?",
+                new String[] { String.valueOf(contact.getId())});
+    }
+
+    public void deleteContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_CONTACTS, KEY_ID + " = ?",
+                new String[] { String.valueOf(contact.getId()) });
+        db.close();
+    }
+}
